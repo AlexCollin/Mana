dd7c4437d6d762fd7b5d225aeabad528dcd097d7
Alex Collin <alex@collin.su>
2020-05-13 15:05:58 +0300

    Delete deps from umbrella mod

--- mix.exs
+++ mix.exs
@@ -12,18 +12,7 @@ defmodule Mana.MixProject do
 
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
-      {:phoenix_html, "~> 2.14"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:plug_cowboy, "~> 2.2"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, "~> 0.15"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
+
     ]
   end
 
